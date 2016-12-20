4aa01774e0889e0035f721d0cc5ea812743292db
zimzim62000 <zimzim62000@gmail.com>
2016-12-20 09:51:50 +0100

    add project symfony 3 default with foundation 6

---
+++ config.rb
@@ -0,0 +1,8 @@
+add_import_path "node_modules/foundation-sites/scss"
+add_import_path "node_modules/foundation-sites/_vendor"
+
+http_path = "web/"
+css_dir = "app/Resources/public/css"
+sass_dir = "app/Resources/public/scss"
+images_dir = "app/Resources/public/images"
+javascripts_dir = "app/Resources/public/js"

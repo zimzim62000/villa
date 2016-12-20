42fbd8f1f7e9ce82a95851c329c22ad870f7a0ef
zimzim62000 <zimzim62000@gmail.com>
2016-12-20 12:20:51 +0100

    update

---
+++ src/AppBundle/Controller/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HomeController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('AppBundle:Home:index.html.twig', array(
+            // ...
+        ));
+    }
+
+}

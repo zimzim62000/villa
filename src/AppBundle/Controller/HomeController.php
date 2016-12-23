e44090f4458c0058492b1698accd4cbe135a75ff
zimzim62000 <zimzim62000@gmail.com>
2016-12-23 15:50:02 +0100

    update

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Contact;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends Controller
 {
@@ -12,5 +14,39 @@ class HomeController extends Controller
             // ...
         ));
     }
+
+    public function costAction()
+    {
+        return $this->render('AppBundle:Home:cost.html.twig', array(
+            // ...
+        ));
+    }
+
+
+    public function contactAction(Request $request)
+    {
+        $contact = new Contact();
+        $form = $this->createForm('AppBundle\Form\ContactType', $contact);
+
+
+        if($request->getMethod() === "POST") {
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($contact);
+                $em->flush($contact);
+
+                return $this->redirectToRoute('app_home_index');
+            }
+        }
+
+        return $this->render('AppBundle:Home:contact.html.twig', array(
+            'contact' => $contact,
+            'form' => $form->createView(),
+        ));
+
+    }
+
 
 }
